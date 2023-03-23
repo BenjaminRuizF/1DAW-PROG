1ee6e3375a7225ceaeee12826d00bba052028511
b.ruiz <b.ruiz@2DAW-13.2SMR.GSD.local>
2023-03-23 14:03:50 +0100

    parte de lectura casi completada

--- CsvToDb-bruiz/src/main/java/LecturaCsv/Lectura.java
+++ CsvToDb-bruiz/src/main/java/LecturaCsv/Lectura.java
@@ -2,17 +2,30 @@ package LecturaCsv;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Lectura {
+
 	public void leerFichero(String fichero) {
+		String array[]=new String[115];
 		try {
 			File f = new File(fichero);
-			Scanner inputFile = new Scanner( f );
-
+			Scanner inputFile = new Scanner(f);
+			int i = 00;
+			while (inputFile.hasNext()) {
+				String line = inputFile.nextLine();
+				array[i] = line;
+				i++;
+			}
 			inputFile.close();
 		} catch (FileNotFoundException e) {
 			System.err.println("ERROR: no se puede abrir el fichero");
 		}
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+
 		}
+	}
 }
--- CsvToDb-bruiz/src/main/java/net/benja/practicas/CsvToDb_bruiz/App.java
+++ CsvToDb-bruiz/src/main/java/net/benja/practicas/CsvToDb_bruiz/App.java
@@ -1,14 +1,18 @@
 package net.benja.practicas.CsvToDb_bruiz;
 
+import LecturaCsv.Lectura;
+
 /**
  * Hello world!
  *
  */
 public class App {
 	private static final int NUMARGS = 5;
+	static Lectura l1 = new Lectura();
 	public static void main(String[] args) {
 		if (args.length != NUMARGS) {
 			System.err.println("La cantidad de argumentos indicados son diferentes a los necesario");
 		}
+		l1.leerFichero(args[4]);
 	}
 }
