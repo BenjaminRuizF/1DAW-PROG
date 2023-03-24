a1c3717842db2dd557238197f61178f40d9744a4
b.ruiz <b.ruiz@2DAW-13.2SMR.GSD.local>
2023-03-24 13:52:22 +0100

    Cambios en CsvToDb

--- CsvToDb-bruiz/src/main/java/LecturaCsv/Lectura.java
+++ CsvToDb-bruiz/src/main/java/LecturaCsv/Lectura.java
@@ -2,30 +2,48 @@ package LecturaCsv;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.Scanner;
 
+import alumnos.Alumnos;
+
 public class Lectura {
+	private Alumnos alumnos[] = new Alumnos[1000];
+	private String array[] = new String[1000];
 
 	public void leerFichero(String fichero) {
-		String array[]=new String[115];
 		try {
 			File f = new File(fichero);
 			Scanner inputFile = new Scanner(f);
-			int i = 00;
+			int i = 0;
 			while (inputFile.hasNext()) {
 				String line = inputFile.nextLine();
 				array[i] = line;
 				i++;
 			}
+			separarLineas();
 			inputFile.close();
 		} catch (FileNotFoundException e) {
 			System.err.println("ERROR: no se puede abrir el fichero");
 		}
-		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
+		
 
-		}
 	}
+
+	public void separarLineas() {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] != null) {
+				String separador[] = array[i].split(",");
+				String curso = separador[0];
+				int numClase = Integer.parseInt(separador[1]);
+				String id1 = separador[2];
+				alumnos[i] = new Alumnos(curso, numClase, id1);
+			}
+		}
+		for (int i = 0; i < alumnos.length; i++) {
+			if(alumnos[i]!=null)
+			System.out.println(alumnos[i].toString());
+		}
+
+	}
+
 }
--- CsvToDb-bruiz/src/main/java/alumnos/Alumnos.java
+++ CsvToDb-bruiz/src/main/java/alumnos/Alumnos.java
@@ -1,11 +1,20 @@
 package alumnos;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 public class Alumnos {
+	private String curso;
+	private int numClase;
+	private String id1;
+	public Alumnos(String curso, int numClase, String id1) {
+		this.curso = curso;
+		this.numClase = numClase;
+		this.id1 = id1;
+	}
+	@Override
+	public String toString() {
+		return "Alumnos [curso=" + curso + ", numClase=" + numClase + ", id1=" + id1 + "]";
+	}
 	
-	
-}
+
+	}
 	
