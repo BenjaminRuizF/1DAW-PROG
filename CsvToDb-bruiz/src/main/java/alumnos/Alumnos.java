008e8984a58d580ee14c0d61645c205487416a88
b.ruiz <b.ruiz@2DAW-13.2SMR.GSD.local>
2023-03-28 13:59:27 +0200

    Terminando clase alumnos

--- CsvToDb-bruiz/src/main/java/alumnos/Alumnos.java
+++ CsvToDb-bruiz/src/main/java/alumnos/Alumnos.java
@@ -1,5 +1,10 @@
 package alumnos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Alumnos {
 	private String curso;
@@ -10,6 +15,22 @@ public class Alumnos {
 		this.numClase = numClase;
 		this.id1 = id1;
 	}
+	  public static void conexion(String Ip, String nombrePdb, String usuario, String passwd) {
+	        try {
+	            Connection conn1 = DriverManager.getConnection( "jdbc:oracle:thin:@//"+Ip+"/"+nombrePdb+"," +usuario+"," +passwd );
+	            jdbcDemo( conn1 );
+	        } catch ( SQLException e ) {
+	            System.err.println( "Las BBDD no son lo tuyo: " + e.getMessage() );
+	        }
+	    }
+
+	    private static void jdbcDemo( Connection conn ) throws SQLException {
+	        Statement stmt = conn.createStatement();
+	        ResultSet resultSet = stmt.executeQuery( "SELECT * FROM prueba_tb" );	       
+	        resultSet.close();
+	        stmt.close();
+	        conn.close();
+	    }
 	@Override
 	public String toString() {
 		return "Alumnos [curso=" + curso + ", numClase=" + numClase + ", id1=" + id1 + "]";
--- CsvToDb-bruiz/src/main/java/net/benja/practicas/CsvToDb_bruiz/App.java
+++ CsvToDb-bruiz/src/main/java/net/benja/practicas/CsvToDb_bruiz/App.java
@@ -1,7 +1,7 @@
 package net.benja.practicas.CsvToDb_bruiz;
 
 import LecturaCsv.Lectura;
-
+import alumnos.Alumnos;
 /**
  * Hello world!
  *
@@ -9,10 +9,13 @@ import LecturaCsv.Lectura;
 public class App {
 	private static final int NUMARGS = 5;
 	static Lectura l1 = new Lectura();
+	Alumnos profe = new Alumnos("1ASIR", 0, "bruiz");
 	public static void main(String[] args) {
 		if (args.length != NUMARGS) {
 			System.err.println("La cantidad de argumentos indicados son diferentes a los necesario");
 		}
 		l1.leerFichero(args[4]);
+		profe.co
+		
 	}
 }
