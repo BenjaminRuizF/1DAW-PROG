049ff9330b62e6790199e1ea990be3ca81f468ee
b.ruiz <b.ruiz@2DAW-13.2SMR.GSD.local>
2023-05-03 13:32:53 +0200

    cambios en csvToDb

--- CsvToDb-bruiz/src/main/java/LecturaCsv/Lectura.java
+++ CsvToDb-bruiz/src/main/java/LecturaCsv/Lectura.java
@@ -9,27 +9,34 @@ import alumnos.Alumnos;
 public class Lectura {
 	private Alumnos[] args;
 	private String fichero;
+	private String curso;
+	private int numClase;
+	private String id1;
+	
 	public Lectura(String fichero) {
 		this.fichero = fichero;
 		 args = new Alumnos[1000];
 	}
 	
-	{
-	try {
-		File f = new File(fichero);
-		Scanner inputFile = new Scanner( f );
-		int num = 0;
-		while (inputFile.hasNext()) {
-			
-			String[] line = inputFile.nextLine().split(",");
-			int numClase = Integer.parseInt(line[1]);
-			args[num] = new Alumnos(line[0],numClase,line[2]);
-			
-		}
+{
+		try {
+			File f = new File(fichero);
+			Scanner inputFile = new Scanner( f );
 
-		inputFile.close();
-	} catch (FileNotFoundException e) {
-		System.err.println("ERROR: no se puede abrir el fichero");
-	}
+			while (inputFile.hasNext()) {
+				String line = inputFile.nextLine();
+			}
+
+			inputFile.close();
+		} catch (FileNotFoundException e) {
+			System.err.println("ERROR: no se puede abrir el fichero");
+		}
 }
+	public void separarLineas(String linea) {
+		String[] ficheroSeparado = linea.split(",");
+		curso = ficheroSeparado[0];
+		numClase = Integer.parseInt(ficheroSeparado[1]);
+		id1 = ficheroSeparado[2];
+		
+	}
 }
